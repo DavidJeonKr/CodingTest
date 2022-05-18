f2799a4ade82b9ac145189523829541a55710369
DavidJeonKr <dudgh7410@naver.com>
2022-05-18 22:22:16 +0900

    124 나라의 숫자

--- Programmers/src/Solution0.java
+++ Programmers/src/Solution0.java
@@ -1,10 +1,12 @@
-public class Sum_of_factors {
+public class Solution0 {
     public static void main(String[] args) {
 
-        System.out.println(solution(12));
+//        System.out.println(solution(12));
+        Solusion1 s1 = new Solusion1();
+        System.out.println(s1.solution(10)); //41
     }
 
-
+        //약수의 합
         public static int solution(int n ) {
             int answer = 0;
 
@@ -15,4 +17,8 @@ public class Sum_of_factors {
             }
             return answer;
         }
+
+
 }
+
+
