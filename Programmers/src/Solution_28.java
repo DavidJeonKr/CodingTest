77263ee46e26300b0c6035e30728d30399139229
YeongHo Jeon <yeonghojeon@YeongHoui-MacBook-Pro.local>
2022-06-07 22:14:24 +0900

    .

--- Programmers/src/Solution_28.java
+++ Programmers/src/Solution_28.java
@@ -2,7 +2,7 @@
 public class Solution_28 {
     public int removeElement(int[] nums, int val) {
         int[] answer = new int[nums.length];
-        
+
 
         for (int i = 0; i < nums.length; i++) {
             if(nums[i] == val) {
